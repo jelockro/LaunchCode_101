3aed60ba39fec948d02509bc0bde2d3c0615cae7
Josh <jlockrow@gmail.com>
2018-10-10 22:27:42 -0500

    adds autoprefixer and postcss-loader to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const autoprefixer = require('autoprefixer');
+
 module.exports = [{
   entry: './app.scss',
   output: {
@@ -18,11 +20,18 @@ module.exports = [{
           },
           { loader: 'extract-loader' },
           { loader: 'css-loader'  },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()]
+            }
+          },
           { loader: 'sass-loader',
             options: {
               includePaths: ['./node_modules']
             }
-          },
+          }
+
         ]
       }
     ]
