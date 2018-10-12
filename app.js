04efe78dd590acc3000d79bf6a8a731d1175b328
Josh <jlockrow@gmail.com>
2018-10-11 21:28:36 -0500

    instantiate @material drawer js

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-import {MDCRipple} from '@material/ripple/index';
+import {MDCRipple} from "@material/ripple/index";
+import {MDCRipple} from "@material/list";
+
+const list = MDCList.attachTo(document.querySelector('.mdc-list'));
+list.wrapFocus = true;
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
