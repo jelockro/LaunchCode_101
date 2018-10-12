1569f058feddcc5726386600fab9248fb9f0f58b
Josh <jlockrow@gmail.com>
2018-10-11 21:59:23 -0500

    fixes javascript instantiating

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import {MDCRipple} from "@material/ripple/index";
-import {MDCRipple} from "@material/list";
+const ripple = new MDCRipple(document.querySelector('.foo-button'));
 
+import {MDCList} from "@material/list";
 const list = MDCList.attachTo(document.querySelector('.mdc-list'));
 list.wrapFocus = true;
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
+import {MDCDrawer} from "@material/drawer";
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+
+
 
