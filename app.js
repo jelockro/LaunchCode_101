3da070c9f7ef2fc6981b5fff1c4792e5d9439676
Josh <jlockrow@gmail.com>
2018-10-15 21:43:09 -0500

    Links js import statements to material design components index.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import {MDCRipple} from "@material/ripple/index";
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
 
-import {MDCList} from "@material/list";
+import {MDCList} from "@material/list/index";
 const list = MDCList.attachTo(document.querySelector('.mdc-list'));
 list.wrapFocus = true;
 
-import {MDCDrawer} from "@material/drawer";
+import {MDCDrawer} from "@material/drawer/index";
 const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 
 
