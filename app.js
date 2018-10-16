2afe75e894f28ffdc3782ad1455bc03c2cc16e80
Josh <jlockrow@gmail.com>
2018-10-15 21:56:14 -0500

    Feat: Adds links drawer opening to navigation icon on top-app-bar
    previously when this was attempted, the id of the top-app-bar header was missing, and that is why the javascript did not function.
    Everythin seems to function like it should now.  However, there is still a css bug that is causing a vertical scroll bar to appear.
    And the top-app-bar seems to have a left padding or margin that needs to be fixed.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import {MDCRipple} from "@material/ripple/index";
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
 
-import {MDCList} from "@material/list/index";
-const list = MDCList.attachTo(document.querySelector('.mdc-list'));
-list.wrapFocus = true;
-
 import {MDCDrawer} from "@material/drawer/index";
 const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 
+import {MDCTopAppBar} from "@material/top-app-bar/index";
+const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+topAppBar.setScrollTarget(document.getElementById('main-content'));
+topAppBar.listen('MDCTopAppBar:nav', () => {
+  drawer.open = !drawer.open;
+});
 
 
