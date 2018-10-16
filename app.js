156c2dd6858d3e9db5b1fd79a966a93a6b9f9f36
Josh <jlockrow@gmail.com>
2018-10-15 23:17:58 -0500

    Feat: creates modal drawer for mobile first design
    Since I will be designing the app for mobile first,
    and the material design guidelines are to use mdc-drawer--modal for mobile
    designs web designs.  I wanted this to be where I start.
    I will need to add some css breakpoints and change the class of the drawer
    depending on the screen-size.

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 
 import {MDCTopAppBar} from "@material/top-app-bar/index";
 const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
-topAppBar.setScrollTarget(document.getElementById('main-content'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
   drawer.open = !drawer.open;
 });
 
+const listEl = document.querySelector('.mdc-drawer .mdc-list');
+const mainContentEl = document.querySelector('.main-content');
 
+listEl.addEventListener('click', (event) => {
+  drawer.open = false;
+});
+
+document.body.addEventListener('MDCDrawer:closed', () => {
+  mainContentEl.querySelector('input, button').focus();
+});
