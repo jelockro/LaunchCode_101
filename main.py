aa3c3a65b6c7fae963c3b3e76264f67b80ed6beb
Josh <jlockrow@gmail.com>
2018-10-18 12:51:43 -0500

    feat: adds template features
    instead of using the form in main.py, creates form.html in templates folder and uses jinja2 to render templates.  also, added dependency module for caesar.py.

--- main.py
+++ main.py
@@ -1,46 +1,30 @@
-from flask import Flask
-from caesar import rotate_string
+from flask import Flask, request
+import helpers
+from caesar import encrypt
 
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 app = Flask(__name__)
 app.config['DEBUG'] = True
-form = '''
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <style>
-            form {
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }
-            textarea {
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }
-        </style>
-    </head>
-    <body>
-      <!-- create your form here -->
-        <form action="" id="usrform" method="POST">
-            Rotate by: 
-            <input type="text" name="rot" value="0"><br>
-             <textarea name="text" rows="10" cols="70" form="usrform"></textarea>
-            <input type="submit" value="Submit Request">
-        </form>
-       
-    </body>
-</html>
-'''
 
 
 
-@app.route("/")
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return form
+    template = jinja_env.get_template('form.html')
+    return template.render()
+
+''' def encrpyt():
+    if request.method == 'POST':
+        rot = request.form.get['rot']
+        text = request.form['textarea']
+        encryptedText = encrypt(text, rot)
+    return  
+'''
 
 app.run()
