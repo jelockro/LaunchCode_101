f25c158c7b249889ba42af1a36ee76ae54e43cc5
Josh <jlockrow@gmail.com>
2018-10-27 22:32:13 -0500

    Form posts Username data with some validation
    the only validation right now is to escape the input to prevent
    injection hacks, and redirects with an error if the username is left
    blank.  I am keeping this pretty rudimetary, although I know there is room
    to break my index.html into more templates. However, for now I am just going to build
    this out one piece simply, and then refactor after if necessary.

--- main.py
+++ main.py
@@ -7,7 +7,21 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
+@app.route("/signup", methods=['POST'] )
+def signup():
+    errors = []
+    error = "{0} can't be left blank"
+    username = request.form['Username']
+    #print(username)
+    #print(request.form)
+    if username:
+        username_escaped = cgi.escape(username)
+    else:
+        errors.append('username_error')
+        return redirect("/?error=" + error.format('Username') )
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    encoded_error = request.args.get("error")
+    return render_template('index.html', error=encoded_error)
 app.run()
