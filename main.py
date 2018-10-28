e1f5ecafd4cc46a7529e875a75c1142013003639
Josh <jlockrow@gmail.com>
2018-10-27 21:44:03 -0500

    adds index router for root

--- main.py
+++ main.py
@@ -7,5 +7,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
-
+@app.route("/")
+def index():
+    return '<h1>Hello World</h1>'
 app.run()
