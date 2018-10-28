936ea7dd28a4e2f3f7ef1346d6cfca4870a322f3
Josh <jlockrow@gmail.com>
2018-10-27 22:53:58 -0500

    Ccreates dictionary of input field data for dry error reporting
    I want to be able to iterate through the values and then provide the proper
    error reporting.  I think using a dictionary could be helpful because of the get
    funciton that allows a default to be used which might save a few lines when there
    are null's in the dict values.

--- main.py
+++ main.py
@@ -9,16 +9,26 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
 @app.route("/signup", methods=['POST'] )
 def signup():
+    print(request.form)
+    input_fields = { 'Username': '', 'Password': '',
+    'Email (optional)': ''}
     errors = []
-    error = "{0} can't be left blank"
-    username = request.form['Username']
+    blank_error = "{0} can't be left blank"
+    for key in request.form:
+        if key in input_fields:
+            input_fields[key] = request.form[key]
+    print(input_fields)
+
     #print(username)
     #print(request.form)
+    username = input_fields['Username']
     if username:
         username_escaped = cgi.escape(username)
+        return render_template('success.html', username=username_escaped)
     else:
         errors.append('username_error')
-        return redirect("/?error=" + error.format('Username') )
+        return redirect("/?error=" + blank_error.format('Username') )
+
 
 @app.route("/")
 def index():
