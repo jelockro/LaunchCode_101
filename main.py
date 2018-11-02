7ab23d51946256f04cebf5914cbdd6a75a6a1aad
Josh <jlockrow@gmail.com>
2018-11-01 23:43:57 -0500

    fixes database configuration, database can now be accessed

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
 import cgi
 import os
-import jinja2
-from flask_sqlalchemy import SQLAlchemy
 
+import jinja2
 app =Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:user@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
-tasks =[]
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -18,4 +28,5 @@ def index():
 
     return render_template('todos.html', title="Get It Done", tasks=tasks)
 
-app.run()
+if __name__ == "__main__":
+    app.run()
