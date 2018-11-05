5946f4975b933f0ce15873a0a99f8d8382eb734e
jelockro <jlockrow@gmail.com>
2018-11-05 02:20:48 -0600

    Feat: Adds error handling

--- main.py
+++ main.py
@@ -25,16 +25,31 @@ blogs = []
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def index():
-
+    title_error = ''    
+    body_error = ''
+    errors = []
     if request.method == 'POST':
-        post_title = request.form['title']
-        post_body = request.form['body']
+        post_title = cgi.escape(request.form['title'])
+        if post_title == '':
+            title_error = "Please fill in the title"
+            errors.append(title_error)
+        
+        post_body = cgi.escape(request.form['body'])
+        if post_body == '':
+            body_error = "Please fill in the body"
+            errors.append(body_error)
+        
+        if errors:
+            return render_template("blog_form.html", title=post_title, body=post_body, title_error=title_error, body_error=body_error )
+        
         new_post = Post(post_title, post_body)
         db.session.add(new_post)
         db.session.commit()
+
         return redirect('/blog')
     else:
-        return render_template('blog_form.html', title="Buld a Blog!")
+        return render_template('blog_form.html', title="Build a Blog!")
+
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
