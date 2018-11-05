c1cae527780325df908333932e79780123a237a7
jelockro <jlockrow@gmail.com>
2018-11-04 22:23:34 -0600

    adds app/sqlalchemy configurations

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ import jinja2
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog:user@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+
+
+
 
 blogs = []
 
