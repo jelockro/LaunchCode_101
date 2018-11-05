f9f4382be1819d6d4672bc581a84494ca6764c54
jelockro <jlockrow@gmail.com>
2018-11-04 22:40:54 -0600

    Fix: Fixes sqlalchemy configuration so that app can access mysql db

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
-import config
 import cgi
 import jinja2
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog:user@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:user@localhost:3306/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-class Blog(db.model):
+class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(256))
