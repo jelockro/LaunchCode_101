5bbb445c7e92020af1f4c4c3e391770922f2c447
Josh <jlockrow@gmail.com>
2018-11-05 18:43:00 -0600

    feat: Adds User class and creates one to many relationship user to posts

--- main.py
+++ main.py
@@ -15,11 +15,22 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(256))
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'), nullable=False)
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
 
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    posts = db.relationship('Post', backref='owner', lazy=True) 
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 blogs = []
 
