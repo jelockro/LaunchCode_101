b177e57d81ac0787748ffa1f00b792cc7733cc7b
Josh <jlockrow@gmail.com>
2018-11-12 20:54:34 -0600

    basic login router and template

--- main.py
+++ main.py
@@ -83,8 +83,10 @@ def post():
     return render_template('post_template.html', post=post)
 
 @app.route('/login')
+def login():
     return render_template('login.html')
 @app.route('/signup')
+def signup():
     return render_template('signup.html')
 
 if __name__ == "__main__":
